19d1a7da979566ea8029c004b72d3232f0c747db
Githubnewdeveloper <37742600+Githubnewdeveloper@users.noreply.github.com>
2019-03-28 22:30:21 +0800

    2.0

--- do_not_use_selenium_as_name.py
+++ do_not_use_selenium_as_name.py
@@ -2,6 +2,8 @@
 
 from selenium import webdriver
 
+import time
+
 #导入谷歌游览器的插件，文件路径cmd+opt+c
 driver = webdriver.Chrome('/Users/apple/Desktop/Programing/pulgin/chromedriver')
 
@@ -9,4 +11,20 @@ driver = webdriver.Chrome('/Users/apple/Desktop/Programing/pulgin/chromedriver')
 driver.fullscreen_window()
 
 #打开一个网站
-driver.get('http://www.google.com')
+driver.get('http://www.baidu.com')
+
+#模拟人类的鼠标点击动作
+#先要在Beatiful Soup中找到link的名字，注意只有element不是列表的才能点击
+#driver.find_element_by_name("tj_trnews").click()
+
+#直接用链接上的文字（网页上的）
+#driver.find_element_by_link_text('hao123').click()
+
+#使用网页检查中的小箭头可以快速定位元素
+driver.find_element_by_id('kw').send_keys('ddos')
+#自动搜索
+driver.find_element_by_id('su').click()
+#让它睡两秒钟，不然太快就关掉了
+time.sleep(3)
+
+driver.quit()
